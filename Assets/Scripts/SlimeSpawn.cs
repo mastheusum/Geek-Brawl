fffb14b2bcd2fc92bf207ae3941eab265bf64083
Lucas Vanderlei <lucasvanderlei.f@gmail.com>
2022-02-03 21:33:31 -0300

    Added Slime animation, Spawn script

---
+++ Assets/Scripts/SlimeSpawn.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeSpawn : MonoBehaviour
+{
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 2.0f, 2.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn(){
+        GameObject obj;
+        obj = Instantiate(prefab, transform.position, Quaternion.identity);
+    }
+}
